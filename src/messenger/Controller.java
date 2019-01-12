df813268a1db19e8bf772ac308176ddd98748c1a
ciscoff <ciscoff@mail.ru>
2019-01-12 18:45:46 +0300

    рабочая версия

--- src/messenger/Controller.java
+++ src/messenger/Controller.java
@@ -1,4 +1,45 @@
 package messenger;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    Button btnSend;
+
+    @FXML
+    Button btnClose;
+
+    @FXML
+    Button btnHide;
+
+    @FXML
+    TextField messageField;
+
+    @FXML
+    TextArea chatField;
+
+    @FXML
+    public void sendMessage() {
+        chatField.appendText(messageField.getText() + "\n");
+        messageField.clear();
+        messageField.requestFocus();
+    }
+
+    @FXML
+    public void closeWindow(){
+        Platform.exit();
+    }
+
+    @FXML
+    void minimizeWindow(){
+        Stage stage = (Stage) btnHide.getScene().getWindow();
+        stage.setIconified(true);
+
+    }
 }
--- src/messenger/Main.java
+++ src/messenger/Main.java
@@ -5,17 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private final int FrameWidth = 450;
+    private final int FrameHeight = 600;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../messenger/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../messenger/ChatGui.fxml"));
+
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("Geekbrains chat");
+        primaryStage.setScene(new Scene(root, FrameWidth, FrameHeight));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
