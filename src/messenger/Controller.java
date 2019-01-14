7ed17d4b91d38da33f9b1cbd2e4d15b8396ade45
ciscoff <ciscoff@ya.ru>
2019-01-14 21:58:16 +0300

    Помещаем сообщения в отдельные HBox и раскрашиваем

--- src/messenger/Controller.java
+++ src/messenger/Controller.java
@@ -1,43 +1,72 @@
+/**
+ * Иконки
+ * https://www.flaticon.com/free-icon/chat_134806
+ */
+
+
 package messenger;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Controller {
+    private boolean alignment = true;
 
     @FXML
     Button btnSend;
-
     @FXML
     Button btnClose;
-
     @FXML
     Button btnHide;
-
     @FXML
     TextField messageField;
-
     @FXML
-    TextArea chatField;
+    VBox chatMessages;
+    @FXML
+    ScrollPane chatScroll;
 
     @FXML
     public void sendMessage() {
-        chatField.appendText(messageField.getText() + "\n");
+        // Формируем контейнер для сообщения
+        Label message = new Label(messageField.getText());
+        message.setStyle("-fx-background-color: #2f4f4f;" +
+                "-fx-background-radius: 10;" +
+                "-fx-border-radius: 10;");
+
+        HBox hb = new HBox();
+
+        hb.setStyle("-fx-padding: 0;" +
+                "-fx-border-color: transparent;" +
+                "-fx-background-color: white;" +
+                "-fx-border-width: 0px;" +
+                "-fx-alignment: " + (alignment ? Pos.CENTER_LEFT : Pos.CENTER_RIGHT) + ";" +
+                "-fx-spacing: 5;");
+        alignment = !alignment;
+        hb.getChildren().add(message);
+        HBox.setHgrow(message, Priority.ALWAYS);
+
+        // Добавляем контейнер сообщения в скроллинг
+        chatMessages.getChildren().add(hb);
+        chatScroll.setVvalue(1.0);
+
         messageField.clear();
         messageField.requestFocus();
     }
 
+
     @FXML
-    public void closeWindow(){
+    public void closeWindow() {
         Platform.exit();
     }
 
     @FXML
-    void minimizeWindow(){
+    void minimizeWindow() {
         Stage stage = (Stage) btnHide.getScene().getWindow();
         stage.setIconified(true);
 
